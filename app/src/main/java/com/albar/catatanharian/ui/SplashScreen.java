3dcc04b94d9f6d1208f17188baa9870553d9d1d7
albarfikri <fikrialbar4@gmail.com>
2021-08-12 21:21:58 +0700

    finish creating login layout

--- app/src/main/java/com/albar/catatanharian/ui/SplashScreen.java
+++ app/src/main/java/com/albar/catatanharian/ui/SplashScreen.java
@@ -3,24 +3,45 @@ package com.albar.catatanharian.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.albar.catatanharian.MainActivity;
 import com.albar.catatanharian.R;
 
+import java.io.File;
+
 public class SplashScreen extends AppCompatActivity {
+
+    public static final String FILENAME = "login";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-
         int timeDelayed = 3000;
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
-                startActivity(intent);
+                if (isLogin()) {
+                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
+                    startActivity(intent);
+                }
                 finish();
             }
         }, timeDelayed);
+    }
+
+    boolean isLogin() {
+        File sdcard = getFilesDir();
+        File file = new File(sdcard, FILENAME);
+        if (file.exists()) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
